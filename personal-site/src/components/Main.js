3875b97153db488b3ea2582a06173d7baba65683
kanganna <anna.f.carandang@gmail.com>
2019-01-18 17:58:19 -0500

    Made sidebar sticky
    main content's margin-left needs to be the sidebar container's width + 1%

--- personal-site/src/components/Main.js
+++ personal-site/src/components/Main.js
@@ -11,15 +11,20 @@ export class Main extends Component {
 			<div className = {styles.mainContent}>
 			
 				<div>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium odio in fermentum sollicitudin. Suspendisse mi neque, scelerisque vitae odio id, interdum ultricies mauris. Nullam nulla orci, ultricies ac maximus eu, tristique sed libero. Nulla sagittis tincidunt est, ut finibus velit finibus vitae. Aliquam elit nisl, dapibus at ante quis, posuere dapibus dui. Fusce sit amet risus augue. Vestibulum suscipit quis mauris malesuada bibendum. Integer interdum, nisi congue ornare feugiat, ligula est pulvinar odio, at accumsan arcu risus eget risus. Suspendisse nulla nulla, fermentum quis lacinia et, mattis at tellus. Mauris ut hendrerit dolor, eu interdum risus. Proin gravida, lectus at congue consequat, dui ipsum feugiat enim, non posuere elit diam id erat.
-
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium odio in fermentum sollicitudin. Suspendisse mi neque, scelerisque vitae odio id, interdum ultricies mauris. Nullam nulla orci, ultricies ac maximus eu, tristique sed libero. Nulla sagittis tincidunt est, ut finibus velit finibus vitae. Aliquam elit nisl, dapibus at ante quis, posuere dapibus dui. Fusce sit amet risus augue. Vestibulum suscipit quis mauris malesuada bibendum. Integer interdum, nisi congue ornare feugiat, ligula est pulvinar odio, at accumsan arcu risus eget risus. Suspendisse nulla nulla, fermentum quis lacinia et, mattis at tellus. Mauris ut hendrerit dolor, eu interdum risus. Proin gravida, lectus at congue consequat, dui ipsum feugiat enim, non posuere elit diam id erat.
+</p>
+<p>
 Phasellus tortor tellus, hendrerit id sodales a, sollicitudin sit amet dui. Duis non mi sollicitudin, auctor erat non, finibus purus. Duis nisl nibh, molestie non dictum imperdiet, venenatis at ex. Fusce ut nulla fringilla, aliquam tortor eu, pellentesque felis. Fusce euismod erat vitae ipsum tristique ullamcorper. Maecenas sit amet felis tellus. Sed fermentum mi ut mauris tempus imperdiet. Suspendisse porttitor nec nisi id elementum. Donec tincidunt in lorem vel sollicitudin. Suspendisse venenatis fermentum neque at malesuada.
-
+</p>
+<p>
 Integer et gravida diam, quis maximus mauris. Maecenas turpis diam, pellentesque sit amet luctus ut, aliquet nec ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque quis orci id dui fermentum consectetur quis vitae tortor. Donec egestas sodales dolor ac viverra. Praesent varius felis enim, vitae imperdiet purus egestas nec. Sed rutrum erat id ante scelerisque venenatis. Curabitur semper dapibus sapien, id dignissim sapien ultricies at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus tincidunt luctus magna, non semper nulla mollis quis.
-
+</p>
+<p>
 Proin maximus tincidunt dolor imperdiet dapibus. Pellentesque neque libero, rutrum non ex non, venenatis congue est. Aliquam ullamcorper, metus et ullamcorper facilisis, lectus felis egestas velit, id feugiat nisi odio vitae nunc. Quisque eleifend justo lorem, vel porttitor lorem posuere et. Nunc molestie sit amet enim in tempor. Etiam porttitor purus enim, non maximus lorem scelerisque sed. Pellentesque non lectus maximus, porta neque non, tempor orci.
-
+</p>
+<p>
 Nunc venenatis dignissim sapien. Curabitur eget diam risus. Aenean placerat sed quam sodales tincidunt. Pellentesque molestie lorem tellus. Phasellus auctor, ante id ornare tempus, diam nulla interdum orci, quis interdum libero orci in nulla. Proin imperdiet sem vitae elit imperdiet, vel volutpat augue bibendum. Nam vulputate neque a lacus commodo, a sodales ipsum suscipit. Phasellus eget rhoncus velit. Sed facilisis velit vitae aliquet interdum. Duis eu orci quis elit vehicula euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate neque sit amet elit molestie fermentum. Sed convallis arcu arcu, nec posuere urna gravida at. Mauris varius tristique nibh, ac elementum dui luctus at. Aenean ipsum lectus, tincidunt nec hendrerit at, convallis vel eros.
+</p>
 				</div>
 				
 			</div>
